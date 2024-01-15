80a88be03209e724007ab0fe28a670a466ac8c60
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-15 11:29:56 -0800

    Fix typo

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import mockData from "./mock-data";
 // It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
 export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
-  const locations = [...new Set(extractedLocations);]
+  const locations = [...new Set(extractedLocations)];
   return locations;
 }
 
