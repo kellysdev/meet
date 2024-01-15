33f8ff49df496b4fc3faa4e9b6d3a3c12f6e7223
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-15 08:22:38 -0800

    Render CitySearch component, add beforeEach() to test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 
 const App = () => {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
