15daa58a999d4fd3dddf57317ddb1b1ae7bb0277
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-16 11:31:23 -0800

    showDetails state, triggered by button, will expand or collapse details.  ARIA attribute included.

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,29 @@
-const Event = () => {
+import { useState } from "react";
+
+const Event = ({ events }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  const [ariaExpanded, setAriaExpanded] = useState(false);
+
+  const showHideDetails = (event) => {
+    if(showDetails === false) {
+      setShowDetails(true);
+      setAriaExpanded(true);
+    } else {
+      setShowDetails(false);
+      setAriaExpanded(false);
+    }
+  };
+
   return (
-    <li></li>
+    <li aria-expanded={ariaExpanded}>
+        {showDetails ? 
+          <div></div>
+        : 
+          <div>
+            <button onClick={showHideDetails} className="show-hide__button" aria-label="Show details">Show Details</button>
+          </div>          
+        }
+    </li>
   );
 };
 
