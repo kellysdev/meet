ea1cb08e31f3ce44718c5ee8d664fb23979a10c1
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-17 08:07:17 -0800

    Test and code to show event details after clicking the "Show Details" button

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,11 +1,18 @@
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { getEvents } from "../api";
 import Event from "../components/Event";
 
-describe("<Event /> component", () => {
+describe("<Event /> component Feature 2", () => {
+  let allEvents;
+  let EventComponent;
+  beforeEach(async () => {
+    allEvents = await getEvents();
+    EventComponent = render(<Event event={allEvents[0]} />);
+  });
 
+// Feature 2: Scenario 1
   test("the event component is collapsed by default and include a 'show details' button", () => {
-    const EventComponent = render(<Event />);
     const listItem = EventComponent.queryByRole("listitem");
     const showDetailsButton = EventComponent.queryByRole("button");
     const hiddenDetails = EventComponent.container.querySelector(".event-details");
@@ -14,23 +21,25 @@ describe("<Event /> component", () => {
     expect(showDetailsButton).toBeInTheDocument();
     expect(showDetailsButton).toHaveClass("show-details");
   });
-
-  test("renders event title", async () => {
-    const allEvents = await getEvents();
-    const EventComponent = render(<Event event={allEvents[0]} />);
+  // render event info when collapsed
+  test("renders event title", () => {
     expect(EventComponent.queryByText(allEvents[0].summary)).toBeInTheDocument();
   });
 
-  test("renders event start time", async () => {
-    const allEvents = await getEvents();
-    const EventComponent = render(<Event event={allEvents[0]} />);
+  test("renders event start time", () => {
     expect(EventComponent.queryByText(allEvents[0].created)).toBeInTheDocument();
   });
 
-  test.only("renders event location", async () => {
-    const allEvents = await getEvents();
-    const EventComponent = render(<Event event={allEvents[0]} />);
+  test("renders event location", () => {
     expect(EventComponent.queryByText(allEvents[0].location)).toBeInTheDocument();
   });
-
+// Feature 2: Scenario 2
+  test("the event details are visible after clicking the 'show details' button", async () => {
+    const user = userEvent.setup();
+    const showDetailsButton = EventComponent.getByText("Show Details");
+    const eventDetails = EventComponent.container.querySelector(".event-details");
+    await user.click(showDetailsButton);
+    // expect(eventDetails).toBeInTheDocument();
+    expect(EventComponent.getByText("Hide Details")).toBeInTheDocument();
+  });
 });
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
   const [ariaExpanded, setAriaExpanded] = useState(false);
@@ -17,6 +18,10 @@ const Event = ({ event }) => {
         <p className="event-location">{event.location}</p>
         {showDetails ? 
           <div className="event-details" aria-expanded={ariaExpanded}>
+            <h3><b>About event:</b></h3>
+            <a href={event.htmlLink}>See details on Google Calendar</a>
+            <p>{event.description}</p>
+            <button onClick={toggleDetails} className="hide-details" aria-label="Hide details">Hide Details</button>
           </div>
         : 
           <button onClick={toggleDetails} className="show-details" aria-label="Show details">Show Details</button>
