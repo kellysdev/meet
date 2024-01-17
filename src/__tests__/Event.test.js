2fcb844513b18d3175a46b8e6168bc1bf4b98be4
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-17 06:29:11 -0800

    Title renders

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -3,12 +3,9 @@ import { getEvents } from "../api";
 import Event from "../components/Event";
 
 describe("<Event /> component", () => {
-  let EventComponent;
-  beforeEach(() => {
-    EventComponent = render(<Event />);
-  });
 
   test("the event component is collapsed by default and include a 'show details' button", () => {
+    const EventComponent = render(<Event />);
     const listItem = EventComponent.queryByRole("listitem");
     const showDetailsButton = EventComponent.queryByRole("button");
     const hiddenDetails = EventComponent.container.querySelector(".event-details");
@@ -18,9 +15,10 @@ describe("<Event /> component", () => {
     expect(showDetailsButton).toHaveClass("show-details");
   });
 
-  test("renders event title", async () => {
+  test.only("renders event title", async () => {
     const allEvents = await getEvents();
-    EventComponent.rerender(<Event event={allEvents[0]} />);
+    const EventComponent = render(<Event event={allEvents[0]} />);
     expect(EventComponent.queryByText(allEvents[0].summary)).toBeInTheDocument();
   });
+  
 });
