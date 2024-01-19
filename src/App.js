2ca0ea07ab840b5a5130f11789dbfe6c819a2811
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-19 06:47:27 -0800

    Formatting, removed incorrect import

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
-import { getEvents, allLocations, extractLocations } from './api';
+import { getEvents, extractLocations } from './api';
 
 import './App.css';
 
 const App = () => {
-  const [events, setEvents] = useState([]);
   // NOE = number of events
+  const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
@@ -16,9 +16,8 @@ const App = () => {
   const fetchData = async () => {
     const allEvents = await getEvents();
     const filteredEvents = currentCity === "See all cities" ? 
-      allEvents :
+      allEvents : 
       allEvents.filter(event => event.location === currentCity);
-
     setEvents(filteredEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
   };
