dcb117206493961e9480ef857d215209544336fc
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-27 10:23:07 -0800

    Use beforeAll and afterAll to clean up code

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,16 +1,29 @@
 import puppeteer from "puppeteer"
+import { escape } from "querystring";
 
 describe("show/hide an event details", () =>{
-  test("An event element is collapsed by default", async () => {
-    const browser = await puppeteer.launch();
+  let browser;
+  let page;
 
-    const page = await browser.newPage();
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
     await page.goto("http://localhost:3000/");
-
     await page.waitForSelector(".event");
+  });
 
+  afterAll(() => {
+    browser.close();
+  })
+  test("An event element is collapsed by default", async () => {
     const eventDetails = await page.$(".event .event-details");
     expect(eventDetails).toBeNull();
-    browser.close();
+  });
+
+  test("User can expand an event to see its details", async () => {
+    await page.click(".event .show-details");
+
+    const eventDetails = await page.$(".event .event-details");
+    expect(eventDetails).toBeDefined();
   });
 });
