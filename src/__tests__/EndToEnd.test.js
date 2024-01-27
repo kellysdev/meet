3952a7b6818fa1810243fa7f5fa19dc32edf4dc3
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-27 14:20:56 -0800

    add test for Feature 1: Filter Events by City

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,6 +1,51 @@
 import puppeteer from "puppeteer"
-import { escape } from "querystring";
 
+// Feature 1
+describe("filter events by city", () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250,
+      timeout: 0,
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event");
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test("when a user hasn't selected a city, show events from all cities", async () => {
+    const eventListLength = await page.evaluate(() => {
+      return document.querySelectorAll(".event").length;
+    });
+    expect(eventListLength).toBe(32);
+  });
+
+  test("user should be able to see a list of suggestions when they search for a city", async () => {
+    await page.type(".city", "Berlin");
+    await page.waitForSelector(".suggestion-item");
+    const suggestion = await page.$(".suggestion-item");
+    expect(suggestion).toBeDefined();
+  }, 17000);
+
+  test("user can select a city from the suggested list", async () => {
+    await page.click(".suggestion-item");
+
+    const eventListLength = await page.evaluate(() => {
+      return document.querySelectorAll(".event").length;
+    });
+    expect(eventListLength).toBe(16);
+  });
+
+});
+
+// Feature 2
 describe("show/hide an event details", () =>{
   let browser;
   let page;
@@ -14,7 +59,8 @@ describe("show/hide an event details", () =>{
 
   afterAll(() => {
     browser.close();
-  })
+  });
+
   test("An event element is collapsed by default", async () => {
     const eventDetails = await page.$(".event .event-details");
     expect(eventDetails).toBeNull();
