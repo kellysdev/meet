a3a68f6ce0c837529e4c72deb0cb1b58cebbd8a5
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-27 10:26:52 -0800

    End to end test for Feature 2: Scenario 3: user can collapse an event to hide details

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -22,8 +22,13 @@ describe("show/hide an event details", () =>{
 
   test("User can expand an event to see its details", async () => {
     await page.click(".event .show-details");
-
     const eventDetails = await page.$(".event .event-details");
     expect(eventDetails).toBeDefined();
+  });
+
+  test("User can collapse an event to hide details", async () => {
+    await page.click(".event .hide-details");
+    const eventDetails = await page.$(".event .event-details");
+    expect(eventDetails).toBeNull();
   });
 });
