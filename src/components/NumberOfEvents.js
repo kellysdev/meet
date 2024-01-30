214fe66add098e976fbd6e7c02a241f6f2d915ac
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-30 15:47:04 -0800

    setErrorAlert if NOE input receives NaN or negative number

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,7 +1,13 @@
-const NumberOfEvents = ({ setCurrentNOE }) => {
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
 
   const handleChange = (e) => {
     setCurrentNOE(e.target.value);
+    if (isNaN(e.target.value) || e.target.value <= 0) {
+      setCurrentNOE(32);
+      setErrorAlert("Please input a whole number for the number of events to display.");
+    } else {
+      setCurrentNOE(32);
+    }
   };
 
   return (
