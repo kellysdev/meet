1594f1ea80d1b14ff5cb55ef11b098078722b516
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-30 14:38:04 -0800

    Import InfoAlert, create infoAlert state and pass setInfoAlert to CitySearch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 import { getEvents, extractLocations } from "./api";
+import { InfoAlert } from "./components/Alert";
 
 import "./App.css";
 
@@ -12,6 +13,7 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
 
   const fetchData = async () => {
     const allEvents = await getEvents();
@@ -44,11 +46,16 @@ const App = () => {
             <CitySearch 
               allLocations={allLocations} 
               setCurrentCity={setCurrentCity}
+              setInfoAlert={setInfoAlert}
             />
             <NumberOfEvents setCurrentNOE={setCurrentNOE} />
           </div>
+
+          <div className="alerts-container">
+            {InfoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+          </div>
         </div>
-        
+
         <EventList events={events} />
       </div>
     )}
