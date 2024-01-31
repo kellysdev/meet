6632092f7066b4e88a36c314c4c8e341c0006dcf
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-31 15:54:23 -0800

    import and implement WarningAlert when navigator is offline

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 import { getEvents, extractLocations } from "./api";
-import { InfoAlert, ErrorAlert } from "./components/Alert";
+import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 
 import "./App.css";
 
@@ -16,6 +16,7 @@ const App = () => {
 
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   const fetchData = async () => {
     const allEvents = await getEvents();
@@ -27,6 +28,11 @@ const App = () => {
   };
 
   useEffect(() => {
+    if (navigator.onLine) {
+      setWarningAlert("");
+    } else {
+      setWarningAlert("You are currently offline.  Be aware that the displayed event data may not be up to date.");
+    }
     fetchData();
   }, [currentCity, currentNOE]);
 
@@ -57,6 +63,7 @@ const App = () => {
         <div className="alerts-container">
             {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
             {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+            {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
         </div>
 
         <EventList events={events} />
