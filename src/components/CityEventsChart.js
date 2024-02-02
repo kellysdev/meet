5f431bf657ef92392ef9e0540787028b0fa8c701
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-02 14:57:06 -0800

    Styling

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -39,7 +39,7 @@ const CityEventsChart = ({ allLocations, events }) => {
       <XAxis type="category" dataKey="city" name="City" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14}} />
       <YAxis type="number" dataKey="count" name="Number of Events" />
       <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-      <Scatter name="A school" data={data} fill="#8884d8" />
+      <Scatter name="Events Per City" data={data} fill="#8DD1E1" />
     </ScatterChart>
   </ResponsiveContainer>
   );
