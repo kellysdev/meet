cd5a5cf0e5af47ad8e4a161db556e27dc59966e3
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-01 17:30:13 -0800

    Initial scatterplot of NOE per city

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -24,6 +24,25 @@ const CityEventsChart = ({ allLocations, events }) => {
     })
     return data;
   };
+
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+    <ScatterChart
+      margin={{
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 20
+      }}
+    >
+      <CartesianGrid />
+      <XAxis type="category" dataKey="city" name="City" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14}} />
+      <YAxis type="number" dataKey="count" name="Number of Events" />
+      <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+      <Scatter name="A school" data={data} fill="#8884d8" />
+    </ScatterChart>
+  </ResponsiveContainer>
+  );
 };
 
 export default CityEventsChart;
