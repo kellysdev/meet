92a14ff9f1e216fae2b85a0efd8867a3047abf0b
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-01 17:44:21 -0800

    Some sizing changes to scatter plot

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -26,13 +26,13 @@ const CityEventsChart = ({ allLocations, events }) => {
   };
 
   return (
-    <ResponsiveContainer width="99%" height={400}>
+    <ResponsiveContainer width="99%" height={300}>
     <ScatterChart
       margin={{
         top: 20,
         right: 20,
-        bottom: 20,
-        left: 20
+        bottom: 60,
+        left: 0
       }}
     >
       <CartesianGrid />
